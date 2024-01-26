db461bf433103f188701758f923549e3e5a4def9
Miguel Ryan Dantas de Freitas <miguelryanfreitas@gmail.com>
2024-01-25 21:39:05 -0300

    minor changes: icons config
    Agora não é mais um botão, temos um ícone para indicar o tema em questão.

--- www/js/script.js
+++ www/js/script.js
@@ -75,14 +75,18 @@ CssMode.addEventListener("click",  (e) => {
     let css = document.getElementById("css");
     let logo1 = document.getElementById("logo1");
     let logo2 = document.getElementById("logo2");
+    let img = document.getElementById("sun");
+
     if (css.getAttribute("href") === "css/darklogin.css") {
         css.setAttribute("href", "css/lightlogin.css");
         logo1.setAttribute("src", "img/logoemporiolight.png");
         logo2.setAttribute("src", "img/logoemporiolight.png");
+        img.setAttribute("src", "img/moon.png");
     } else if (css.getAttribute("href") === "css/lightlogin.css") {
         css.setAttribute("href", "css/darklogin.css");
         logo1.setAttribute("src", "img/logoemporio.png");
         logo2.setAttribute("src", "img/logoemporio.png");
+        img.setAttribute("src", "img/sun.png");
     }
 })
 
